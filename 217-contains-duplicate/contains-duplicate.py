1b31adae216dae00e71ae4a12203773c743cb6b5
Syed Mujtaba Hussain <135517051+sayyedmujtaba@users.noreply.github.com>
2025-03-25 21:37:57 +0500

    Time: 15 ms (40.84%) | Memory: 26 MB (43.91%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,4 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        return len(nums) != len(set(nums))
+        heapset= set()
+        for n in nums:
+            if n in heapset:
+                return True
+            heapset.add(n)
+        return False
